5fcfe936709450484881e8b398acf81144a6327f
Govind Mohan <gov@supercluster.dev>
2023-11-19 11:18:32 -0500

    feat: replace content

--- src/lib/Home.svelte
+++ src/lib/Home.svelte
@@ -2,22 +2,16 @@
 	<div
 		class="text-center md:text-left font-bold text-slate-200 text-4xl md:text-5xl mb-8 md:mb-16 w-full"
 	>
-		Get curated show <br class="hidden md:block" /> and movie recommendations
-		<br class="hidden md:block" /> with Open AI
+		Track your internet rabbitholes
+		<br class="hidden md:block" /> and hold onto the knowledge you discover
 	</div>
 	<div class="flex flex-wrap items-center mr-auto">
-		<button
-			on:click
-			class="w-full md:w-auto text-center bg-pink-600 hover:bg-pink-700 mb-4 flex-none rounded-full text-white font-medium py-3 px-6 mr-8"
-		>
-			Get recommendations
-		</button>
 		<a
-			href="https://www.youtube.com/watch?v=Z3LrfdctOpU"
+			href="https://form.zootools.co/go/tfln5Mfi8mFGVRYk5da8"
 			target="_blank"
 			class="w-full md:w-auto text-center bg-white/20 hover:bg-white/50 mb-4 flex-none rounded-full text-white font-medium py-3 px-6"
 		>
-			Learn how it's built
+                  Subscribe
 		</a>
 	</div>
 </div>
